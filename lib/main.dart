9e57757c5a7620558346bfc6e2ee47c3392dd1a0
QDQD <104239715@qq.com>
2019-09-11 00:54:09 +0800

    底部导航

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,94 @@
-import 'dart:io';
-import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
-
+import 'package:redux/redux.dart';
+import 'package:lssy/splash.dart';
 void main(){
-  runApp(MyApp());
-//  设置状态栏，颜色
-//  SystemUiOverlayStyle systemUiOverlayStyle = SystemUiOverlayStyle(
-//    statusBarColor: Colors.transparent,
-//  );
-//  SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  runApp(App());
 }
+class App extends StatelessWidget {
+  final Store<int> store;
+  final String title;
 
+  App({Key key, this.store,this.title}) : super(key: key);
 
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
+        primaryIconTheme: const IconThemeData(color: Colors.white),
+        brightness: Brightness.light,
+        primaryColor: new Color.fromARGB(255, 0, 215, 198),
+        accentColor: Colors.cyan[300],
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SplashPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MainPage extends StatefulWidget{
+  MainPage({Key key, this.title}) : super(key: key);
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MainPageState createState() => new _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MainPageState extends State<MainPage>{
+
+  BottomNavigationBarItem _buildItem({String icon,String text}){
+    return BottomNavigationBarItem(
+      icon: new Image.asset(
+        icon,
+        width: 35.0,
+        height: 35.0,
+      ),
+      title: Text(
+        text,
+        style: TextStyle(
+          color: Colors.black,
+          fontStyle: FontStyle.normal,
+          fontWeight: FontWeight.bold
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButtonNavBar(){
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      items: [
+        _buildItem(
+          icon: "assets/image/ic_main_tab_find_nor.png",
+          text: "职位"
+        ),
+        _buildItem(
+            icon: "assets/image/ic_main_tab_company_nor.png",
+            text: "公司"
+        ),
+        _buildItem(
+            icon: "assets/image/ic_main_tab_contacts_nor.png",
+            text: "消息"
+        ),
+        _buildItem(
+            icon: "assets/image/ic_main_tab_my_nor.png",
+            text: "我的"
+        )
+      ],
+      onTap: _onSelectTab,
+    );
+  }
+
+  void _onSelectTab(int index) {
+    print("点击底部导航条:"+index.toString());
+  }
 
   @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      appBar: PreferredSize(
-          child: Offstage(
-            offstage: false,
-            child: AppBar(
-              brightness: Brightness.light,//Theme.of(context).accentColor, 设置主题颜色
-              backgroundColor: Color.fromRGBO(241, 245, 251, 1),
-              title: Center(child: Text("Flutter Demo")),
-              elevation: 0,
-            ),
-          ),
-          preferredSize: Size(double.infinity,0),
+  Widget build(BuildContext context){
+    return new Scaffold(
+      body: Center(
+        child: new Text("首页文字"),
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Color.fromRGBO(241, 245, 251, 1)
-        ),
-      )
+      bottomNavigationBar: _buildButtonNavBar()
     );
   }
 }
+
