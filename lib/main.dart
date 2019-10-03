5bd9760aad381219fa1c8773ae9737ea6b3adcc3
QDQD <104239715@qq.com>
2019-10-03 17:09:13 +0800

    主页面更改

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:redux/redux.dart';
 import 'package:lssy/splash.dart';
-import 'package:lssy/layout_type.dart';
-import 'package:lssy/widgets/job_page.dart';
-import 'package:lssy/widgets/company_page.dart';
-import 'package:lssy/widgets/chat_page.dart';
-import 'package:lssy/widgets/mine_page.dart';
 import 'package:lssy/widgets/home_page.dart';
 
 void main(){
@@ -49,108 +44,10 @@ class MainPage extends StatefulWidget{
 }
 
 class _MainPageState extends State<MainPage>{
-  LayoutType _layoutSelection = LayoutType.job;
-
-  Color _colorTabMatching({LayoutType layoutSelection}){
-    return _layoutSelection == layoutSelection ? Colors.cyan[300] : Colors.grey;
-  }
-
-  BottomNavigationBarItem _buildItem({String icon,LayoutType layoutSelection}){
-    String text = layoutName(layoutSelection);
-
-    return BottomNavigationBarItem(
-      icon: new Image.asset(
-        icon,
-        width: 35.0,
-        height: 35.0,
-      ),
-      title: Text(
-        text,
-        style: TextStyle(
-          color: Colors.black,
-          fontStyle: FontStyle.normal,
-          fontWeight: FontWeight.bold
-        ),
-      ),
-    );
-  }
-
-//  Widget _buildButtonNavBar(){
-//    return BottomNavigationBar(
-//      type: BottomNavigationBarType.fixed,
-//      items: [
-//        _buildItem(
-//          icon: _layoutSelection == LayoutType.job
-//              ? "assets/image/ic_main_tab_find_pre.png"
-//              : "assets/image/ic_main_tab_find_nor.png",
-//            layoutSelection: LayoutType.job
-//        ),
-//        _buildItem(
-//            icon: _layoutSelection == LayoutType.company
-//                ? "assets/image/ic_main_tab_company_pre.png"
-//                : "assets/image/ic_main_tab_company_nor.png",
-//            layoutSelection: LayoutType.company
-//        ),
-//        _buildItem(
-//            icon: _layoutSelection == LayoutType.chat
-//                ? "assets/image/ic_main_tab_contacts_pre.png"
-//                : "assets/image/ic_main_tab_contacts_nor.png",
-//            layoutSelection: LayoutType.chat
-//        ),
-//        _buildItem(
-//            icon: _layoutSelection == LayoutType.mine
-//                ? "assets/image/ic_main_tab_my_pre.png"
-//                : "assets/image/ic_main_tab_my_nor.png",
-//            layoutSelection: LayoutType.mine
-//        )
-//      ],
-//      onTap: _onSelectTab,
-//    );
-//  }
-
-  void _onLayoutSelected(LayoutType selection){
-    setState((){
-      _layoutSelection = selection;
-    });
-  }
-
-  void _onSelectTab(int index){
-    switch (index) {
-      case 0:
-        _onLayoutSelected(LayoutType.job);
-        break;
-      case 1:
-        _onLayoutSelected(LayoutType.company);
-        break;
-      case 2:
-        _onLayoutSelected(LayoutType.chat);
-        break;
-      case 3:
-        _onLayoutSelected(LayoutType.mine);
-        break;
-    }
-  }
-
-  Widget _buildBody() {
-    LayoutType layoutSelection = _layoutSelection;
-    print(layoutSelection);
-    switch (layoutSelection) {
-      case LayoutType.job:
-        return HomePage();
-      case LayoutType.company:
-        return CompanyPage();
-      case LayoutType.chat:
-        return ChatPage();
-      case LayoutType.mine:
-        return MinePage();
-    }
-  }
-
   @override
   Widget build(BuildContext context){
     return new Scaffold(
-      body: _buildBody(),
-//      bottomNavigationBar: _buildButtonNavBar()
+      body:  HomePage()
     );
   }
 }
