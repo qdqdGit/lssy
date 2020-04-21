e331a04ef9aabc6b6cf13fb1b55087fdb661e745
QDQD <1042397515@qq.com>
2020-04-21 23:37:35 +0800

    书签页布局更高

--- lib/widgets/bookmark_page.dart
+++ lib/widgets/bookmark_page.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lssy/unit/Adapt.dart';
 import 'package:lssy/widgets/component/PageHeader.dart';
 
 //TODO:书签页
@@ -17,15 +19,44 @@ class _BookmarkPage extends State<BookmarkPage>{
     return new Scaffold(
       backgroundColor: Color.fromARGB(255, 234, 233, 230),
       body: new Container(
-        padding: EdgeInsets.only(top: 10),
-        child: new Row(
+        padding: EdgeInsets.only(top: statusBarHeight,right: Adapt.px(37),bottom: Adapt.px(126)),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
           children: <Widget>[
             new Container(
-              padding: EdgeInsets.only(top: statusBarHeight,left: 20,right: 20),
-              alignment: Alignment.topRight,
-              child: new PageHeader(),
+              height: Adapt.px(238),
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  new Container(
+                    child: new PageHeader(),
+                    height: Adapt.px(128),
+                    alignment: Alignment.centerRight,
+                  ),
+                  new Container(
+                    color: Colors.red,
+                    height: Adapt.px(110),
+                  )
+                ],
+              ),
             ),
-            new Expanded(child: new Text("文本演示"))
+            new Container(
+              height: 100,
+              color: Colors.amber,
+            ),
+            new Container(
+              height: 100,
+              color: Colors.blue,
+            ),
+            new Container(
+              height: 100,
+              color: Colors.deepPurple,
+            ),
+            new Container(
+              height: 100,
+              color: Colors.green,
+            )
           ],
         ),
       ),
